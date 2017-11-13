1c4f950c827c6c52e57a91f189ed4f75d9abf20f
ShengDong Lu <HandsomeDong@users.noreply.github.com>
2017-11-12 23:24:58 -0800

    python script

--- generating.py
+++ generating.py
@@ -2,7 +2,16 @@ from os import listdir
 from os.path import isfile, join
 
 onlyfiles = [ f for f in listdir('./_posts') if isfile(join('./_posts/',f)) ]
-print(onlyfiles)
+categories = set()
 for f in onlyfiles:
     reader = open(join('./_posts/',f))
-    print(reader.readlines())
+    content = reader.readlines()
+    for line in content:
+        if line[:10] == 'categories':
+            categories.add(line[12:].rstrip())
+            break
+print(categories)
+
+output = open('./_data/catagories.yml', '+w')
+for cate in categories:
+    output.write('- name: ' + cate + '\n\n')
