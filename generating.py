9fe9b45a24b8c64a7b6c5bc0ba5cb88bf67964d7
ShengDong Lu <HandsomeDong@users.noreply.github.com>
2017-11-13 21:24:56 -0800

    finish before graph

--- generating.py
+++ generating.py
@@ -1,16 +1,26 @@
 from os import listdir
 from os.path import isfile, join
 
+
+def new_node():
+
+
+
+
 onlyfiles = [ f for f in listdir('./_posts') if isfile(join('./_posts/',f)) ]
 categories = set()
+posts = []
 for f in onlyfiles:
     reader = open(join('./_posts/',f))
     content = reader.readlines()
+    post = {}
     for line in content:
         if line[:10] == 'categories':
             categories.add(line[12:].rstrip())
-            break
-print(categories)
+            post['categories'] = line[12:].rstrip()
+        if line[:5] == 'title':
+            post['title'] = line[7:].rstrip()
+print(posts)
 
 for cate in categories:
     output = open('./_post_categories/'+cate+'.markdown', 'w')
